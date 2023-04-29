1d93bc8b32be074861342a34995adb525aa86db1
Piyush <bhangalepiyush@gmail.com>
2023-04-29 10:57:06 +0530

    chore: add new font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        playfair: ["Playfair Display", "serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
