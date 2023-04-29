d8d0d31a4f52204a30dbca8b0f93b3e693174048
Piyush <bhangalepiyush@gmail.com>
2023-04-29 10:57:15 +0530

    feat: add quote

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,17 @@
 export default function LoginPage() {
   return (
-    <div className="absolute bottom-0 left-0 right-0 top-0 flex h-screen w-full bg-[#EDAE59] bg-[url(/dot_grid.png)] bg-right-bottom bg-no-repeat">
+    <div className="absolute bottom-0 left-0 right-0 top-0 flex h-screen w-full bg-[#EDAE59] bg-[url(/dot_grid.png)] bg-right-bottom bg-no-repeat font-playfair">
       <div className="grid w-full grid-cols-12 gap-4">
-        <div className="border-2xl col-span-6 rounded-r-3xl bg-[#45259A]">
-          .
+        <div className="border-2xl col-span-6 justify-between rounded-tr-3xl bg-[#45259A]">
+          <div className="flex h-full flex-col">
+            <div className="flex flex-1 flex-col items-center justify-center">
+              
+            </div>
+
+            <div className="rounded-tr-3xl bg-[#240C64] px-12 py-24 text-6xl font-bold leading-none tracking-wide text-[#EDAE59]">
+              Start creating and let inspiration follow
+            </div>
+          </div>
         </div>
         <div className="col-span-6">.</div>
       </div>
