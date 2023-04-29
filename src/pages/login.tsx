58423c8e49810b717d231af2b54818164c0ce2a8
Piyush <bhangalepiyush@gmail.com>
2023-04-29 12:29:05 +0530

    refactor: change layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
   return (
     <div className="absolute bottom-0 left-0 right-0 top-0 flex h-screen w-full bg-[#EDAE59] bg-[url(/dot_grid.png)] bg-right-bottom bg-no-repeat font-playfair">
       <div className="grid w-full grid-cols-12 gap-4">
-        <div className="border-2xl col-span-6 justify-between rounded-tr-3xl bg-[#45259A]">
+        <div className="border-2xl col-span-3 justify-between rounded-tr-3xl bg-[#45259A]">
           <div className="flex h-full flex-col">
             <div className="flex flex-1 flex-col items-center justify-center">
               <div className="flex flex-col items-center justify-center gap-8">
