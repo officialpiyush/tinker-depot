9a30e13da23cfb2271f08a9c707fe21288019a0a
Piyush <bhangalepiyush@gmail.com>
2023-04-29 09:35:28 +0530

    refactor: remove old styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,9 @@ import { api } from "~/utils/api";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return <div className="h-screen bg-zinc-900">
+    <Component {...pageProps} />
+  </div>;
 };
 
 export default api.withTRPC(MyApp);
