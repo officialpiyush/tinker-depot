22dc074d68f784ff7944b81ea4774ea6d4f8d992
Piyush <bhangalepiyush@gmail.com>
2023-04-29 15:41:15 +0530

    feat: add sidebar

---
+++ src/components/Sidebar/SidebarLeft.tsx
@@ -0,0 +1,13 @@
+export default function Sidebar() {
+  return (
+    <div className="flex h-full flex-col items-center gap-8 p-8">
+      <div>
+        <div className="h-24 w-24 rounded-full bg-[#602EAF]" />
+      </div>
+
+      <div className="h-full rounded-full bg-[#602EAF] px-3 py-4">
+        <div className="h-12 w-12 rounded-full bg-yellow-500" />
+      </div>
+    </div>
+  );
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,8 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import SidebarLeft from "~/components/Sidebar/SidebarLeft";
+import SidebarRight from "~/components/Sidebar/SidebarRight";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -12,7 +14,18 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <div className="h-screen bg-zinc-900">
-        <Component {...pageProps} />
+        <main className="flex h-screen gap-12 py-4">
+          <div className="flex h-full justify-center overflow-y-auto px-4">
+            <SidebarLeft />
+          </div>
+
+          <div className="h-full w-full flex-1 overflow-y-auto">
+            <Component {...pageProps} />
+          </div>
+
+          <div className="flex h-full justify-center overflow-y-auto px-4">
+          </div>
+        </main>
       </div>
     </SessionProvider>
   );
