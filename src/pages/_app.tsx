fe9d95070a5c70b2fd33b2449675cfd3611ba815
Piyush <bhangalepiyush@gmail.com>
2023-04-29 23:14:31 +0530

    feat: bar on page load

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+import NextNProgress from "nextjs-progressbar";
 
 import "~/styles/globals.css";
 import SidebarLeft from "~/components/Sidebar/SidebarLeft";
@@ -12,13 +13,14 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <NextNProgress />
       <div className="h-screen bg-[#2F2A3B] bg-[url('/dashboard-grid.svg')] bg-center">
         <main className="flex h-screen gap-2 py-4">
           <div className="flex h-full justify-center overflow-y-auto px-4">
             <SidebarLeft />
           </div>
 
-          <div className="h-full w-full flex-1 py-8 pr-8 overflow-y-auto">
+          <div className="h-full w-full flex-1 overflow-y-auto py-8 pr-8">
             <Component {...pageProps} />
           </div>
         </main>
