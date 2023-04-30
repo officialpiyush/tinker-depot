72b6cab8f12887f57e3010c06dc000be1e95e347
Piyush <bhangalepiyush@gmail.com>
2023-04-30 16:55:55 +0530

    query textarea

--- src/pages/query.tsx
+++ src/pages/query.tsx
@@ -1,6 +1,6 @@
 export default function QueryRoom() {
   return (
-    <div className="flex flex-col h-full items-center justify-center gap-8">
+    <div className="flex h-full flex-col items-center justify-center gap-8">
       {new Array(3).fill(0).map((_, i) => (
         <div key={i} className="grid w-full grid-cols-12 gap-4">
           <div className="col-span-6 flex flex-col justify-between gap-24 rounded-xl bg-[#d87d7d] px-8 py-4">
@@ -26,8 +26,12 @@ export default function QueryRoom() {
           ></div>
 
           <div className="col-span-3 flex flex-col">
-            <div className="bg-[#a7cd4f] text-center rounded-xl h-full py-6 px-8">
-                Query
+            <div className="h-full rounded-xl bg-[#a7cd4f] py-4 text-center">
+              Query
+              <textarea
+                className="h-full w-full rounded-xl bg-[#a7cd4f] px-4 pt-4 focus:outline-none text-black placeholder-zinc-500"
+                placeholder="Write queries here do you don't forget in the conversation"
+              ></textarea>
             </div>
           </div>
         </div>
