979236141d02423d3743336683c333f05e64311b
Piyush <bhangalepiyush@gmail.com>
2023-04-30 12:05:02 +0530

    feat: contact creator buttons

--- src/pages/creator/[userId].tsx
+++ src/pages/creator/[userId].tsx
@@ -1,9 +1,18 @@
-import { LucidePersonStanding, LucideUser } from "lucide-react";
+import {
+  LucideCheckCircle,
+  LucideMail,
+  LucideMessageSquare,
+  LucidePersonStanding,
+  LucidePhone,
+  LucideUser,
+  LucideVideo,
+  LucideXCircle,
+} from "lucide-react";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import type { GetServerSideProps } from "next/types";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Chip from "~/components/Chip";
 import { getServerAuthSession } from "~/server/auth";
 
@@ -17,6 +26,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 export default function CreatorPage() {
   const session = useSession();
   const router = useRouter();
+  const [creatorAvailable, setCreatorAvailable] = useState(false);
 
   useEffect(() => {
     if (!session.data) {
@@ -96,11 +106,54 @@ export default function CreatorPage() {
               alt=""
             />
 
-            <div className="mx-4 my-2 bg-[#D9D9D9] px-4 py-2 rounded-md text-center">
+            <div className="mx-4 my-2 rounded-md bg-[#D9D9D9] px-4 py-2 text-center">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
               Consequuntur, maiores nam nisi, sit reiciendis quis rerum enim
               earum et tempora ut ipsum? Ducimus iste quod laboriosam, rem quo
               eligendi dolore.
+            </div>
+          </div>
+
+          {/* contact icons */}
+          <div className="flex">
+            <div className="w-full">Donate</div>
+
+            <div className="flex w-full flex-col items-center gap-4 rounded-xl bg-[#8C78C3]">
+              <div className="flex gap-2 px-4  pt-4 text-[#E7E1F6]">
+                <LucideMail size={20} color="white" />
+                SMS when available
+              </div>
+              <div className="flex w-full flex-col gap-4 rounded-xl bg-[#A7CD4F] px-4 py-4 text-sm font-medium">
+                <div className="flex items-center justify-between gap-2">
+                  <LucideMessageSquare size={20} color="black" />
+                  <div>Whatsapp</div>
+                  {creatorAvailable ? (
+                    <LucideCheckCircle size={20} color="#56672E" />
+                  ) : (
+                    <LucideXCircle size={20} color="#56672E" />
+                  )}
+                </div>
+
+                <div className="flex items-center justify-between gap-2">
+                  <LucideVideo size={20} color="black" />
+                  <div>Video Call</div>
+                  {creatorAvailable ? (
+                    <LucideCheckCircle size={20} color="#56672E" />
+                  ) : (
+                    <LucideXCircle size={20} color="#56672E" />
+                  )}
+                </div>
+
+                <div className="flex items-center justify-between gap-2">
+                  <LucidePhone size={20} color="black" />
+                  <div>Voice Call</div>
+                  {creatorAvailable ? (
+                    <LucideCheckCircle size={20} color="#56672E" />
+                  ) : (
+                    <LucideXCircle size={20} color="#56672E" />
+                  )}
+                </div>
+              </div>
             </div>
           </div>
         </div>
