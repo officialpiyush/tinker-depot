b147153a6f04c68b4b8fa6b903c00d49d7869a0e
Piyush <bhangalepiyush@gmail.com>
2023-04-30 13:48:04 +0530

    feat: toggle, and refetch

--- src/pages/creator/[userId].tsx
+++ src/pages/creator/[userId].tsx
@@ -15,6 +15,7 @@ import { useRouter } from "next/router";
 import type { GetServerSideProps } from "next/types";
 import { useEffect, useState } from "react";
 import Chip from "~/components/Chip";
+import { cn } from "~/lib/utils";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/utils/api";
 
@@ -33,9 +34,12 @@ export default function CreatorPage() {
 
   const { userId } = router.query;
 
-  const { data: creatorData } = api.creators.getCreator.useQuery({
+  const { data: creatorData, refetch: creatorRefetch } = api.creators.getCreator.useQuery({
     userId: userId as string,
   });
+  const registerForNotificationMutation =
+    api.creators.addToNotification.useMutation();
+  const statusMutation = api.creators.toggleAvailability.useMutation();
 
   useEffect(() => {
     if (!session.data) {
@@ -44,12 +48,35 @@ export default function CreatorPage() {
     }
   }, [session, router]);
 
+  useEffect(() => {
+    if (creatorData?.available !== undefined) {
+      setCreatorAvailable(creatorData.available);
+    }
+  }, [creatorData]);
+
+  const registerForNotificaton = async () => {
+    await registerForNotificationMutation.mutateAsync({
+      userId: session.data?.user.id || "ghost",
+    });
+
+    alert("Done!");
+  };
+
+  const toggleStatus = async () => {
+    await statusMutation.mutateAsync({
+      available: !creatorData?.available,
+    });
+    
+    
+    void creatorRefetch();
+  };
+
   return (
     <div className="grid h-full grid-cols-12 gap-4">
       <div className=" col-span-8">
         <div className="flex h-full flex-col gap-4 ">
           {/* top bar */}
-          <div className="flex items-end justify-between">
+          <div className="flex items-center justify-between">
             {/* about the author */}
             <div className="flex w-fit items-center gap-2 rounded-3xl rounded-bl-none bg-[#D9D9D9] py-2 pl-2 pr-4">
               <div className="rounded-full bg-[#714F4F] p-2">
@@ -72,6 +99,18 @@ export default function CreatorPage() {
               >
                 {creatorData?.available ? "Available" : "Unavailable"}
               </div>
+            </div>
+
+            <div
+              className={cn(
+                session.data?.user.id !== userId && "hidden",
+                "flex items-center"
+              )}
+              onClick={() => void toggleStatus()}
+            >
+              <button className="rounded-md bg-[#EDAE59] px-4 py-2 text-sm hover:bg-opacity-90">
+                Toggle Status
+              </button>
             </div>
           </div>
 
@@ -143,10 +182,14 @@ export default function CreatorPage() {
             </div>
 
             <div className="flex w-full flex-col items-center gap-4 rounded-xl bg-[#8C78C3]">
-              <div className="flex gap-2 px-4  pt-4 text-[#E7E1F6]">
+              <button
+                onClick={() => void registerForNotificaton()}
+                disabled={creatorData?.available}
+                className="flex gap-2 px-4 pt-4  text-[#E7E1F6] disabled:cursor-not-allowed"
+              >
                 <LucideMail size={20} color="white" />
                 SMS when available
-              </div>
+              </button>
               <div className="flex w-full flex-col gap-6 rounded-xl bg-[#A7CD4F] px-4 py-6 text-sm font-medium">
                 <div className="flex items-center justify-between gap-2">
                   <LucideMessageSquare size={20} color="black" />
